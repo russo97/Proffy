a63b3f3a4cc6c4ce57da0ba54c526fee72bf023d
Wanderson Silva <wandersonsilva768@gmail.com>
2020-08-13 04:16:03 -0300

    exibindo Landing Page no App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Landing from './src/pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Changed project name</Text>
+    <>
+      <Landing />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
